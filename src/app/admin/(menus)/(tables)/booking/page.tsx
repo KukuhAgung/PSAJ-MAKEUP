691db85ffa350ef1a67149435c1ffc08ba2efbd7
KukuhAgung <130808713+KukuhAgung@users.noreply.github.com>
2025-04-15 04:13:31 +0700

    feat : error vercel 3

---
+++ src/app/admin/(menus)/(tables)/booking/page.tsx
@@ -0,0 +1,13 @@
+
+import PageBreadcrumb from "@/components/organism/common/PageBreadCrumb";
+
+import React from "react";
+
+export default function BasicTables() {
+  return (
+    <div>
+      <PageBreadcrumb pageTitle="Task Kanban" />
+
+    </div>
+  );
+}
---
+++ src/app/admin/(menus)/(tables)/transaction-history/page.tsx
@@ -0,0 +1,12 @@
+
+import PageBreadcrumb from "@/components/organism/common/PageBreadCrumb";
+
+import React from "react";
+
+export default function BasicTables() {
+  return (
+    <div>
+      <PageBreadcrumb pageTitle="Task Kanban" />
+    </div>
+  );
+}
